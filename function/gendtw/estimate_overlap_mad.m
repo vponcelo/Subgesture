c72b4856b8c06833af991f8f89b97dcb1e026af0
hugojair <hugo.jair@gmail.com>
2015-04-16 17:12:07 -0500

    modified function, it works over labeled sequences

--- function/gendtw/estimate_overlap_mad.m
+++ function/gendtw/estimate_overlap_mad.m
@@ -1,7 +1,7 @@
-function [O1,O2,R] = estimate_overlap_mad(GT,PREDSEQ,perc)
+function [O1,O2,R,overlap] = estimate_overlap_mad(GT,PREDSEQ,perc)
 
 %% O1 - Without considering the iddle gesture as class
-%% O1 - Considering the iddle gesture as class
+%% O2 - Considering the iddle gesture as class
 %% perc - threshold on overlap to be considered as recognized
 
 fpth=110; %% number of frames to consider a gesture as detected (false pos.)
@@ -10,10 +10,26 @@ O1=zeros(1,length(clx)-1);
 O2=zeros(1,length(clx));
 DETT=zeros(1,length(clx));
 
+GTtestkFr=zeros(size(GT));
+detSeqLog=zeros(size(GT));
+
+
 for i=1:length(clx),
+    
+    
+%     overlapok
     
     ofin=find(GT==clx(i));
     ofin2=find(PREDSEQ==clx(i));
+    
+    cGTtestkFr =GTtestkFr;
+    cdetSeqLog=detSeqLog;
+    
+    cGTtestkFr(ofin)=1;
+    cdetSeqLog(ofin2)=1;
+    
+    ovs(i) = sum(cGTtestkFr & cdetSeqLog)./sum(cGTtestkFr | cdetSeqLog);   
+
     
     nofin4=setdiff(1:length(GT),ofin);
     
@@ -21,6 +37,7 @@ for i=1:length(clx),
     DETT(i)=round(length(inter2)./fpth);
         
     inte=intersect(ofin,ofin2);
+
     if length(inte)./length(ofin) > perc,
         O2(i)=1;
     end
@@ -35,4 +52,5 @@ R.prec2=sum(O2)./(sum(O2)+sum(DETT));
 R.rec=sum(O1)./(length(clx)-1);
 R.rec2=sum(O2)./(length(clx));
 
+overlap=mean(ovs);
 end
