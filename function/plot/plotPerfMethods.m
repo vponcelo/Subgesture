6f147305289591bc19461cc7dd36fc39591ed79e
vponcelo <vponce@ub.edu>
2015-08-14 12:03:28 +0200

    Plot error plots with the performance of methods

--- function/plot/plotPerfMethods.m
+++ function/plot/plotPerfMethods.m
@@ -1,37 +1,55 @@
 function plotPerfMethods(baseline,v1,t1,v2,t2,v3,t3,v4,t4,v5,t5)
+
     % function that plots the performance for the different methods over 10 runs of the GA
 
     if ~exist('baseline','var')
-        error('plotExecs:errVar','Baseline varaible must exist');
+        error('plotExecs:errVar','Baseline variable must exist');
     end
     x = 1:1:10;
     baseline = repmat(baseline,1,10);
     figure,
-    subplot(3,2,1);plot(x,baseline,'k-',x,min(v1),'bv',x,max(v1),'b^',x,mean(v1),'b-',x,min(t1),'rv',x,max(t1),'r^',x,mean(t1),'r-')
-    hold on
-    title('HMM');
+    subplot(5,2,1); errorbar(x,mean(v1),std(v1),'b'); %boxplot(v1);
+    hold on;
+    title('Error bar HMM validation');
+    hold off;
+    subplot(5,2,2); errorbar(x,mean(t1),std(t1),'r'); %boxplot(t1);
+    hold on;
+    title('Error bar HMM test');
+    hold off;
+    subplot(5,2,3); errorbar(x,mean(v2),std(v2),'b'); %boxplot(v2);
+    hold on;
+    title('Error bar SRM-HMM validation');
+    hold off;
+    subplot(5,2,4); errorbar(x,mean(t2),std(t2),'r'); %boxplot(t2);
+    hold on;
+    title('Error bar SRM-HMM test');
+    hold off;
+    subplot(5,2,5); errorbar(x,mean(v3),std(v3),'b'); %boxplot(v3);
+    hold on;
+    title('Error bar DTW validation');
+    ylabel('Mean score and standard deviations');
+    hold off;
+    subplot(5,2,6); errorbar(x,mean(t3),std(t3),'r'); %boxplot(t3);
+    hold on;
+    title('Error bar DTW test');
+    ylabel('Mean score and standard deviations');
+    hold off;
+    subplot(5,2,7); errorbar(x,mean(v4),std(v4),'b'); %boxplot(v4);
+    hold on;
+    title('Error bar SRM-DTW validation');
+    hold off;
+    subplot(5,2,8); errorbar(x,mean(t4),std(t4),'r'); %boxplot(t4);
+    hold on;
+    title('Error bar SRM-DTW test');
+    hold off;
+    subplot(5,2,9); errorbar(x,mean(v5),std(v5),'b'); %boxplot(v5);
+    hold on;
+    title('Error bar SRM-SVM validation');
     xlabel('Generation');
-    ylabel('Score');
-    legend('VideoDarwin','maxVal','minVal','meanVal','maxTest','minTest','meanTest','Location','SE');
-    subplot(3,2,2);plot(x,baseline,'k-',x,min(v2),'bv',x,max(v2),'b^',x,mean(v2),'b-',x,min(t2),'rv',x,max(t2),'r^',x,mean(t2),'r-')
-    title('SRM-HMM');
+    hold off;
+    subplot(5,2,10); errorbar(x,mean(t5),std(t5),'r'); %boxplot(t5);
+    hold on;
+    title('Error bar SRM-SVM test');
     xlabel('Generation');
-    ylabel('Score');
-    legend('VideoDarwin','maxVal','minVal','meanVal','maxTest','minTest','meanTest','Location','SE');
-    subplot(3,2,3);plot(x,baseline,'k-',x,min(v3),'bv',x,max(v3),'b^',x,mean(v3),'b-',x,min(t3),'rv',x,max(t3),'r^',x,mean(t3),'r-')
-    title('DTW');
-    xlabel('Generation');
-    ylabel('Score');
-    legend('VideoDarwin','maxVal','minVal','meanVal','maxTest','minTest','meanTest','Location','SE');
-    subplot(3,2,4);plot(x,baseline,'k-',x,min(v4),'bv',x,max(v4),'b^',x,mean(v4),'b-',x,min(t4),'rv',x,max(t4),'r^',x,mean(t4),'r-')
-    title('SRM-DTW');
-    xlabel('Generation');
-    ylabel('Score');
-    legend('VideoDarwin','maxVal','minVal','meanVal','maxTest','minTest','meanTest','Location','SE');
-    subplot(3,2,5);plot(x,baseline,'k-',x,min(v5),'bv',x,max(v5),'b^',x,mean(v5),'b-',x,min(t5),'rv',x,max(t5),'r^',x,mean(t5),'r-')
-    title('SRM-SVM');
-    xlabel('Generation');
-    ylabel('Score');
-    legend('VideoDarwin','maxVal','minVal','meanVal','maxTest','minTest','meanTest','Location','SE');
-    hold off
+    hold off;
 end
